753fedc54561cd465f23049be771d853d691bf93
FratelloDog <123020177+FratelloDog@users.noreply.github.com>
2023-01-19 16:28:11 +0100

    Update test.java

--- Test2/src/test2/test.java
+++ Test2/src/test2/test.java
@@ -2,26 +2,24 @@ package test2;
 
 import java.awt.*;
 import javax.swing.*;
-import visualsort.Rectangle;
-import visualsort.Rectangle;
 
-public class test {
+public class test2 {
     JFrame frame = new JFrame("Cane dio");
     JPanel pnl = new JPanel();
-    int TITLEGAP = 40;
-    int WIDTH = 500, HEIGHT = 500 + TITLEGAP;
+    int TITLEGAP = 37;
+    int WIDTH = 504, HEIGHT = 500 + TITLEGAP;
     
-    public test() {
+    public test2() {
         frame.setSize(WIDTH,HEIGHT);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(pnl);
         pnl.setBackground(Color.black);
 //-------------------------------------------------------------------------        
-        int size = 10;
+        int size = 100;
         pnl.setLayout(new GridLayout(1, size)); //  <--------- sesso grande
         int value;
-        int[] arr = new int[10];
+        int[] arr = new int[size];
         
         int i;
         for(i = 1; i <= size; i++)
@@ -31,9 +29,10 @@ public class test {
             value = arr[i];
             pnl.add(new Rectangle(value, WIDTH/size, value*(HEIGHT-TITLEGAP)/size,0, -HEIGHT));
         }
+        frame.pack();
     }
     
     public static void main(String[] args) {
-        new test();
+        new test2();
     }
 }

689fcdecabba1eb3996355bd6921cbdb3b1c21fe
Nikicoraz <49104514+Nikicoraz@users.noreply.github.com>
2023-01-19 17:49:56 +0100

    Bubble sort e insertion sort

--- VisualSort/src/visualsort/Ordinamenti.java
+++ VisualSort/src/visualsort/Ordinamenti.java
@@ -11,6 +11,12 @@ import java.util.Random;
  * @author Nicola Corato
  */
 public class Ordinamenti {
+    private static void scambia(int[] arr, int i1, int i2){
+            int temp = arr[i1];
+            arr[i1] = arr[i2];
+            arr[i2] = temp;
+    }
+    
     public static void stampaArray(int[] arr){
         for(int e : arr){
             System.out.println(e);
@@ -22,9 +28,29 @@ public class Ordinamenti {
         for(int i = 0; i < arr.length * 1.5; i++){
             int i1 = r.nextInt(arr.length);
             int i2 = r.nextInt(arr.length);
-            int temp = arr[i1];
-            arr[i1] = arr[i2];
-            arr[i2] = temp;
+            scambia(arr, i1, i2);
         }
+    }
+    
+    public static void bubbleSort(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length - 1 - i; j++){
+                if(arr[j] > arr[j + 1]){
+                    scambia(arr, j, j + 1);
+                }
+            }
+        }
+    }
+    
+    public static void insertionSort(int[] arr){
+        for(int i=0;i<arr.length;++i){
+            int j = i;
+            while(j > 0 && arr[j-1]>arr[j]){
+                int key = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = key;
+                j = j-1; 
+            }
+      }
     }
 }

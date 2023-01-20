12ea4303f4a06b9a885cb2caa4e6863e4a925c98
FratelloDog <123020177+FratelloDog@users.noreply.github.com>
2023-01-20 15:27:39 +0100

    Add files via upload

--- Test2/src/test2/RectangleB.java
+++ Test2/src/test2/RectangleB.java
@@ -13,21 +13,27 @@ public class RectangleB extends JPanel {
     private int rectW;
     private int rectH;
     
-    public RectangleB(int value, int pos, int unit) {
+    private int frameW;
+    private int frameH;
+    
+    public RectangleB(int value, int pos, int unit, int frameW, int frameH) {
         this.unit = unit;
         this.value = value;
-        this.pos = pos++;
+        this.pos = pos;
+        this.frameW = frameW;
+        this.frameH = frameH;
         rectW = unit;
         rectH = (unit * value);
         x = unit * pos;
-        y = (HEIGHT + rectH);
-        this.setBackground(pos%2==0?Color.yellow:Color.darkGray);
+        y = (frameH - rectH);
+        
     }
     
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawRect(x, y, rectW, rectH);
+        this.setBackground(pos%2==0?Color.black:Color.white);
     }
     
     public void E_come_il_debug_ma_non_e_il_debug() {
@@ -35,20 +41,16 @@ public class RectangleB extends JPanel {
                     + "\tUnit: " + unit + "\n"
                     + "Value: " + value + "\tPosition: " + pos + "\n" 
                     + "RectW: " + rectW + "\tRectH: " + rectH + "\n"
-                    + "X: " + x + "\t\tY: " + y + "\n");
+                    + "X: " + x + "\t\tY: " + (y+37) + "\n");
     }
-    
-    
-//    Penso che servano i getX e getY per girare il grafico 
-//    ma 0 voglia di pensarci, buona fortuna programmatori
-//
+
 //    public int getX() {
 //        return x;
 //    }
-//    
-//    public int getY() {
-//        return y;
-//    }
+    
+    public int getY() {
+        return y;
+    }
     
     public int getWidth() {
         return rectW;
--- Test2/src/test2/test2.java
+++ Test2/src/test2/test2.java
@@ -4,55 +4,40 @@ import java.awt.*;
 import javax.swing.*;
 
 public class test2 {
-    JFrame frame = new JFrame("Cane dio");
+    JFrame frame = new JFrame("Grazie Dio");
     JPanel pnl = new JPanel();
-    public int WIDTHerror = 10, HEIGTHerror = 37;
-    public int WIDTH = 504 + WIDTHerror, HEIGHT = 500 + HEIGTHerror;
-    public int size = 500;
+    static int widthAdj = 14, heightAdj = 37;
+    static int WIDTH = 500, HEIGHT = 500;
+    int size = 250;
     
     public test2() throws InterruptedException {
-        frame.setSize(WIDTH,HEIGHT);
+        frame.setSize(WIDTH+widthAdj, HEIGHT + heightAdj);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(pnl);
-        pnl.setBackground(Color.black);
+        frame.setResizable(false);
+        pnl.setBackground(Color.darkGray);
 //-------------------------------------------------------------------------        
-        pnl.setLayout(new GridLayout(1, size)); //  <--------- sesso grande
-        int value;
+        pnl.setLayout(new GridLayout(1, size));
         int[] arr = new int[size];
         
         int i;
         for(i = 1; i <= size; i++)
             arr[i-1] = i;
         
-        int rectX = 0, rectY, rectW, rectH;
-        Ordinamenti.shuffle(arr);
+        //Ordinamenti.shuffle(arr);
         
-        int unit = (WIDTH - WIDTHerror)/size;
-
+        int value;
+        int unit = WIDTH/size;
         for(i = 0; i < size; i++) {
-            value = arr[i];
-            
-            RectangleB rect = new RectangleB(value, i, unit);
+            value = arr[i];         
+            RectangleB rect = new RectangleB(value, i, unit, WIDTH, HEIGHT);
             pnl.add(rect);
             rect.E_come_il_debug_ma_non_e_il_debug();
-
-
-//            rectW = (WIDTH - WIDTHerror)/size;
-//            rectH = (value*(HEIGHT-HEIGTHerror)/size);
-//            rectX += rectW;
-//            rectY = -HEIGHT + rectH;            
-//            pnl.add(new Rectangle(value, rectW, rectH, rectX, rectY));
-//            
-//            System.out.println("------------------------\n"
-//                    + "Value: " + value + "\n"
-//                    + "RectW: " + rectW + "\tRectH: " + rectH + "\n"
-//                    + "RectX: " + rectX + "\tRectY: " + rectY + "\n");
         }
-        
-        Thread.sleep(1000);
-        frame.setSize(WIDTH+1, HEIGHT +1);
-        frame.setSize(WIDTH, HEIGHT);
+        Thread.sleep(1);
+        frame.setSize(WIDTH+1, HEIGHT+1);
+        frame.setSize(WIDTH+widthAdj, HEIGHT+heightAdj);
     }
     
     public static void main(String[] args) throws InterruptedException {

f2e29ce40efa078e8cf523a95f6a1fa5b083a7fc
FratelloDog <123020177+FratelloDog@users.noreply.github.com>
2023-01-27 18:33:46 +0100

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,11 +1,16 @@
 import time
 import random
+import threading
+import math
 
+global velocity
+velocity = [1]
 
 def bubble_sort(array, main, stop):
     n = len(array)
     for i in range(n - 1):
         for j in range(n - i - 1):
+            print(velocity[0])
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
                 main.event_generate("<<draw>>")
@@ -216,7 +221,6 @@ def partition(array, main, stop, inf, sup):
     time.sleep(0.001)
     return i + 1
     
-
 def quick_sort(array, main, stop, inf, sup):
     if (stop[0]):
         return
@@ -224,3 +228,122 @@ def quick_sort(array, main, stop, inf, sup):
         q = partition(array, main, stop, inf, sup)
         quick_sort(array, main, stop, inf, q - 1)
         quick_sort(array, main, stop, q + 1, sup)
+
+def gnome_sort(array, main, stop):
+    # Per ora identico all'insertion sort, quando faremo anche i colori si noterà la differenza
+    n = len(array)
+    pos = 0
+    while(pos < n):
+        if(pos == 0 or array[pos] >= array[pos - 1]):
+            pos += 1
+        else:
+            array[pos], array[pos - 1] = array[pos - 1], array[pos]
+            pos -= 1
+            main.event_generate("<<draw>>")
+            time.sleep(0.001)
+        
+        if(stop[0]):
+            return
+
+def introinsertion(array, main, stop, inf, sup):
+    n = sup - inf + 1
+    for i in range(1, n):
+        j = i
+        while j > 0 and array[inf + j - 1] > array[inf + j]:
+            array[inf + j - 1], array[inf + j] = array[inf + j], array[inf + j - 1]
+            j -= 1
+            main.event_generate("<<draw>>")
+            time.sleep(0.001)
+            if stop[0]:
+                return
+    return
+
+def introheapify(arr, N, i, stop, inf):
+    if (stop[0]):
+            return
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < N and arr[inf + largest] < arr[inf + l]:
+        largest = l
+
+    if r < N and arr[inf + largest] < arr[inf + r]:
+        largest = r
+        
+    if largest != i:
+        arr[inf + i], arr[inf + largest] = arr[inf + largest], arr[inf + i]  # swap
+        introheapify(arr, N, largest, stop, inf)
+
+def introheap(array, main, stop, inf, sup):
+    N = sup - inf + 1
+
+    for i in range(N//2 - 1, -1, -1):
+        introheapify(array, N, i, stop, inf)
+        main.event_generate("<<draw>>")
+        if (stop[0]):
+            return
+
+    for i in range(N-1, 0, -1):
+        array[inf + i], array[inf] = array[inf], array[inf + i]  # swap
+        introheapify(array, i, 0, stop, inf)
+        main.event_generate("<<draw>>")
+        if (stop[0]):
+            return
+
+def introutil(array, main, stop, maxdepth, inf, sup):
+    n = sup - inf + 1
+    #if n < 16:
+    if n < 64:
+        introinsertion(array, main, stop, inf, sup)
+    elif maxdepth == 0:
+        introheap(array, main, stop, inf, sup)
+    else:
+        q = partition(array, main, stop, inf, sup)
+        introutil(array, main, stop, maxdepth - 1, inf, q - 1)
+        introutil(array, main, stop, maxdepth - 1, q + 1, sup)
+
+def intro_sort(array, main, stop):
+    maxdepth = 3
+    #maxdepth = math.floor(math.log2(len(array))) * 2
+    introutil(array, main, stop, maxdepth, 0, len(array) - 1)
+
+def sleep_sort(array: list, main, stop):
+    copia = array.copy()
+    array.clear()
+    
+    e = threading.Event()
+
+    def s_sort(val):
+        e.wait(0.1 * val)
+        array.append(val)
+        main.event_generate("<<draw>>")
+
+    for x in copia:
+        threading.Thread(target=s_sort, args=[x]).start()
+    
+    copia = sorted(copia)
+    uscita = False
+    while array != copia and uscita == False:
+        if stop[0]:
+            e.set()
+            uscita = True
+        time.sleep(0.5)
+
+def shell_sort(array, main, stop):
+    n = len(array)
+    gaps = [701, 301, 132, 57, 23, 10, 4, 1]
+    for gap in gaps:
+        for i in range(gap, n):
+            tmp = array[i]
+            j = i
+            while j >= gap and array[j - gap] > tmp:
+                array[j] = array[j - gap]
+                j -= gap
+                main.event_generate("<<draw>>")
+                time.sleep(0.001)
+                if stop[0]:
+                    return
+            array[j] = tmp
+            main.event_generate("<<draw>>")
+            time.sleep(0.001)
